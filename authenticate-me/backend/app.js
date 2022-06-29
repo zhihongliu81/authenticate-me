59e8c59845c634d06198e69969e7a627e22813d3
zhihong <zhihong.liu81@gmail.com>
2022-06-29 16:46:49 -0500

    Add User authentication Express middleware

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -17,6 +17,9 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 
+const routes = require('./routes');
+
+app.use(routes);
 
 // Security Middleware
 if (!isProduction) {
@@ -42,9 +45,7 @@ app.use(
     })
 )
 
-const routes = require('./routes');
 
-app.use(routes);
 
 // Catch unhandled requests and forward to error handler.
 app.use((_req, _res, next) => {
