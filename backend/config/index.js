bd48b86832a608445f253187998ba3026f319c95
zhihong <zhihong.liu81@gmail.com>
2022-07-01 10:40:48 -0500

    change some files

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,9 +1,10 @@
 module.exports = {
     environment: process.env.NODE_ENV || 'development',
-    port: process.env.port || 5000,
+    port: process.env.PORT || 8000,
     dbFile: process.env.DB_FILE,
     jwtConfig: {
-        secret: process.env.JWT_SECRET,
-        expiresIn: process.env.JWT_EXPIRES_IN
+      secret: process.env.JWT_SECRET,
+      expiresIn: process.env.JWT_EXPIRES_IN
     }
-};
+  };
+
