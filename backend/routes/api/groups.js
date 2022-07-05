b19b75a4448f4575300021bc89be26dc8f6447fa
zhihong <zhihong.liu81@gmail.com>
2022-07-05 16:20:47 -0500

    add get all events of a group specified by its id

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
-const { User, Group, sequelize, Membership, Image } = require('../../db/models');
+const { User, Group, sequelize, Membership, Image, Event, Venue } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
@@ -433,7 +433,61 @@ router.delete('/:groupId/membership/membershipId', restoreUser, requireAuth, asy
 })
 
 
+// Get all events of a group specified by its id
+router.get('/:groupId/events', async (req, res) => {
+    const group = await Group.findByPk(req.params.groupId);
+    if (!group) {
+        res.statusCode = 404;
+        return res.json({
+            "message": "Group couldn't be found",
+            "statusCode": 404
+          });
+    }
 
+    const events = await Event.findAll({
+        where: {
+            groupId: req.params.groupId
+        },
+        include: [
+            {
+                model: Group,
+                attributes: ['id', 'name', 'city', 'state']
+            },
+            {
+                model: Venue,
+                attributes: ['id', 'city', 'state']
+
+            },
+            {
+                model: User,
+                attributes: ['id']
+            }
+        ]
+    });
+
+    let formattedEvents = [];
+    for (let i = 0; i < events.length; i++) {
+        let event = events[i];
+        let formattedEvent = {
+            id: event.id,
+            groupId: event.groupId,
+            venueId: event.venueId,
+            name: event.name,
+            type: event.type,
+            startDate: event.startDate,
+            numAttending: event.Users.length,
+            previewImage: event.previewImage,
+            Group: event.Group,
+            Venue: event.Venue
+        };
+
+        formattedEvents.push(formattedEvent);
+
+    }
+
+    res.json(formattedEvents)
+
+})
 
 
 
