e6200b2e4753603d0a08922ceb8fcd0ef577c270
zhihong <zhihong.liu81@gmail.com>
2022-07-05 16:45:04 -0500

    add get details of an event specified by its id endpoints

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -46,9 +46,72 @@ router.get('/', async (req, res) => {
 
     }
 
-
     res.json(formattedEvents);
 })
+
+
+// Get details of an event specified by its id
+router.get('/:eventId', async (req, res) => {
+    const event = await Event.findByPk(req.params.eventId, {
+        include: [
+            {
+                model: Group,
+                attributes: ['id', 'name', 'private', 'city', 'state']
+            },
+            {
+                model: Venue,
+                attributes: ['id', 'address', 'city', 'state', 'lat', 'lng']
+            },
+            {
+                model: User,
+                attributes: ['id'],
+                through: {
+                    attributes: []
+                }
+            },
+            {
+                model: Image,
+                attributes: ['url']
+            }
+        ],
+        attributes: {
+            exclude: ['createdAt', 'updatedAt', 'previewImage']
+        }
+    });
+
+    if (!event) {
+        res.statusCode = 404;
+        return res.json({
+            "message": "Event couldn't be found",
+            "statusCode": 404
+          })
+    }
+
+    const images = [];
+    for (let i = 0; i < event.Images.length; i++) {
+        images.push(event.Images[i].url);
+    }
+
+    const formattedEvent = {
+            id: event.id,
+            groupId: event.groupId,
+            venueId: event.venueId,
+            name: event.name,
+            description: event.description,
+            type: event.type,
+            startDate: event.startDate,
+            endDate: event.endDate,
+            numAttending: event.Users.length,
+            Group: event.Group,
+            Venue: event.Venue,
+            images
+    };
+
+
+    res.json(formattedEvent);
+})
+
+
 
 
 
