dbf8033cccb663cd62a36ba1277b26209bf5ded2
zhihong <zhihong.liu81@gmail.com>
2022-07-06 16:14:37 -0500

    add edit an event specified by its id endpoint

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -114,7 +114,99 @@ router.get('/:eventId', async (req, res) => {
 })
 
 
+// Edit an event specified by its id
+const validateEvent = [
+    check('venueId')
+    .exists({ checkFalsy: true })
+    .withMessage( "Venue does not exist" ),
+    check('name')
+    .exists({checkFalsy: true})
+    .isLength({min: 5})
+    .withMessage("Name must be at least 5 characters"),
+    check('type')
+    .exists({checkFalsy: true})
+    .isIn(['Online', 'In person'])
+    .withMessage("Type must be Online or In person"),
+    check('capacity')
+    .exists({checkFalsy: true})
+    .isInt()
+    .withMessage( "Capacity must be an integer"),
+    check('price')
+    .exists({checkFalsy: true})
+    .isFloat()
+    .withMessage( "Price is invalid"),
+    check('description')
+    .exists({checkFalsy: true})
+    .withMessage("Description is required"),
+    check('startDate')
+    .isISO8601()
+    .isAfter()
+    .withMessage("Start date must be in the future"),
+    check('endDate')
+    .isISO8601()
+    .custom((value, {req}) => {
+        const startTime = Date.parse(req.body.startDate);
+        const endTime = Date.parse(value);
+        if (endTime <= startTime) {
+            throw new Error("End date is less than start date")
+        }
+        return true;
+    })
+    .withMessage("End date is less than start date"),
+    handleValidationErrors
+]
+router.put('/:eventId', restoreUser, requireAuth, validateEvent, async (req, res) => {
+    const event = await Event.findByPk(req.params.eventId);
+    if (!event) {
+        res.statusCode = 404;
+        return res.json({
+            "message": "Event couldn't be found",
+            "statusCode": 404
+          })
+    }
 
+    const venue = await Venue.findByPk(req.body.venueId);
+    if (!venue) {
+        res.statusCode = 404;
+        return res.json({
+            "message": "Venue couldn't be found",
+            "statusCode": 404
+          })
+    }
+
+    const membership = await Membership.findOne({
+        where: {
+            groupId: event.groupId,
+            memberId: req.user.id
+        }
+    })
+    if (membership && (membership.status === 'organizer' || membership.status === 'co-host')) {
+        const newEvent = await event.update({
+            ...req.body
+        })
+        const formattedEvent = {
+            id: newEvent.id,
+            groupId: newEvent.groupId,
+            venueId: newEvent.venueId,
+            name: newEvent.name,
+            type: newEvent.type,
+            capacity: newEvent.capacity,
+            price: newEvent.price,
+            description: newEvent.description,
+            startDate: newEvent.startDate,
+            endDate: newEvent.endDate
+        }
+
+        res.json(formattedEvent);
+    } else {
+        res.statusCode = 403;
+        return res.json({
+            "message": "Forbidden",
+            "statusCode": 403
+          })
+    }
+
+})
 
 
 module.exports = router;
